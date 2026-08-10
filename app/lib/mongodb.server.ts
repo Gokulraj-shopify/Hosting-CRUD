2bf9846468852caac6be030b9dae13561bca28a1
Gokulraj Srinivasan <gsrinivasan@junctionsolutions.com>
2026-08-10 14:06:49 +0530

    env changes

--- app/lib/mongodb.server.ts
+++ app/lib/mongodb.server.ts
@@ -1,8 +1,12 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 
-if(!uri) {
+if (!uri) {
   throw new Error("MONGODB_URI is not defined in .env file");
 }
 
